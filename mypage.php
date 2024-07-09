6f8e0b85634f513bee56a3b49843f2aca21e4e25
miyanokengo <k.miyano.sys22@morijyobi.ac.jp>
2024-07-09 14:21:29 +0900

    マイページすこし進めた

--- mypage.php
+++ mypage.php
@@ -1,4 +1,3 @@
-<?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,21 +6,55 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-    // DB名は仮
-    $sql = "SELECT * FROM profiles";
-    $result = $conn->query($sql);
+<?php
+session_start();
+require_once 'db_connect.php';
 
-    if ($result->num_rows > 0) {
-        while($row = $result->fetch_assoc()) {
-            echo "<img src='" . $row["harakiri"] . "'>";    
-        }
-    } else {
-        echo "0 results";
+//mysqlに接続
+try {
+    $pdo = new PDO($dsn, $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // エラーモードを例外モードに設定
+
+    echo "データベース {$database} に接続しました。";
+
+    //SQL文の作成
+    $sql = "SELECT * FROM profiles";
+    //プリペアードステートメントの作成
+    $stmt = $pdo->prepare($sql);
+    //SQL文の実行
+    $stmt->execute();
+    //結果の取得
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    //テーブルのタイトル行
+    echo "<table>";
+    echo "<thead><tr>";
+    echo "<th>id</th>";
+    echo "<th>user_id</th>";
+    echo "<th>名前</th>";
+    echo "<th>本文</th>";
+    echo "<th>画像</th>";
+    echo "</tr></thead>";
+    echo "<tbody>";
+
+    foreach ($result as $row) {
+        echo "<tr>";
+        echo "<td>{$row['id']}</td>";
+        echo "<td>{$row['user_id']}</td>";
+        echo "<td>{$row['username']}</td>";
+        echo "<td>{$row['bio']}</td>";
+        echo "<td>{$row['profile_image_url']}</td>";
+        echo "</tr>";
     }
-    
-    $conn->close();
-    ?>
-    
+    echo "</tbody>";
+    echo "</table>";
+} catch (PDOException $e) {
+    echo "エラーが発生しました。";
+    echo $e->getMessage();
+}
+?>
+    <a href="createmypage.php">マイページ編集</a>
+    <a href="follow_list.php">フォロー中</a>
+    <a href="follow_list.php">フォロワー</a>
 </body>
 </html>
+
