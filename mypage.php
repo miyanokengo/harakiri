4e2da8c6e60f341dbe0d37e93e24a5b861887e8c
miyanokengo <k.miyano.sys22@morijyobi.ac.jp>
2024-08-26 10:26:37 +0900

    cssが反映されていなかったので直した

--- mypage.php
+++ mypage.php
@@ -3,59 +3,62 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/mypage.css" type="text/css">
     <title>Document</title>
 </head>
 <body>
+<a href="createmypage.php">マイページ登録・編集</a>
+    <a href="follow_sitai_list.php">フォローしたい人一覧</a>
+    <a href="follow_list.php">フォロー一覧</a>
+    <a href="follower_list.php">フォロワー</a>
 <?php
 session_start();
 require_once 'db_connect.php';
 
+if (isset($_SESSION['user_id'])) {
+    $user_id = $_SESSION['user_id'];
+    echo 'ユーザーIDは: ' . htmlspecialchars($user_id);
+} else {
+    echo 'ユーザーIDが設定されていません。';
+}
+
 //mysqlに接続
-// try {
-//     $pdo = new PDO($dsn, $user, $pass);
-//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // エラーモードを例外モードに設定
+try {
+    $pdo = new PDO($dsn, $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // エラーモードを例外モードに設定
 
-//     echo "データベース {$database} に接続しました。";
+   // echo "データベース {$database} に接続しました。";
 
-//     //SQL文の作成
-//     $sql = "SELECT * FROM profiles";
-//     //プリペアードステートメントの作成
-//     $stmt = $pdo->prepare($sql);
-//     //SQL文の実行
-//     $stmt->execute();
-//     //結果の取得
-//     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-//     //テーブルのタイトル行
-//     echo "<table>";
-//     echo "<thead><tr>";
-//     echo "<th>id</th>";
-//     echo "<th>user_id</th>";
-//     echo "<th>名前</th>";
-//     echo "<th>本文</th>";
-//     echo "<th>画像</th>";
-//     echo "</tr></thead>";
-//     echo "<tbody>";
+    //SQL文の作成
+    $sql = "SELECT * FROM users";
+    //プリペアードステートメントの作成
+    $stmt = $pdo->prepare($sql);
+    //SQL文の実行
+    $stmt->execute();
+    //結果の取得
+    $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    //テーブルのタイトル行
+    echo "<table>";
+    echo "<thead><tr>";
+    echo "<th>名前</th>";
+    echo "<th>説明文</th>";
+    echo "</tr></thead>";
+    echo "<tbody>";
 
-//     foreach ($result as $row) {
-//         echo "<tr>";
-//         echo "<td>{$row['id']}</td>";
-//         echo "<td>{$row['user_id']}</td>";
-//         echo "<td>{$row['name']}</td>";
-//         echo "<td>{$row['bio']}</td>";
-//         echo "<td>{$row['profile_image_url']}</td>";
-//         echo "</tr>";
-//     }
-//     echo "</tbody>";
-//     echo "</table>";
-// } catch (PDOException $e) {
-//     echo "エラーが発生しました。";
-//     echo $e->getMessage();
-// }
+    foreach ($result as $row) {
+        echo "<tr>";
+        echo "<td>{$row['name']}</td>";
+        echo "<td>{$row['bio']}</td>";
+        echo "</tr>";
+    }
+    echo "</tbody>";
+    echo "</table>";
+} catch (PDOException $e) {
+    echo "エラーが発生しました。";
+    echo $e->getMessage();
+}
 ?>
-    <a href="createmypage.php">マイページ登録・編集</a>
-    <a href="follow_sitai_list.php">フォローしたい人一覧</a>
-    <a href="follow_list.php">フォロー一覧</a>
-    <a href="follower_list.php">フォロワー</a>
+    
 
 </body>
 </html>
